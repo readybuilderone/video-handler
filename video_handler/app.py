8bd724a057c0155c43a9b824b1be74c8c0b4989e
LinYe <31615967@qq.com>
2023-07-12 13:31:46 +0800

    添加api gateway主动调用

--- video_handler/app.py
+++ video_handler/app.py
@@ -3,10 +3,7 @@ import os
 import boto3
 
 import logging
-import shlex
 import subprocess
-
-# import requests
 
 logger = logging.getLogger()
 logger.setLevel(logging.INFO)
@@ -14,53 +11,71 @@ logger.setLevel(logging.INFO)
 formatter = logging.Formatter('%(asctime)s %(levelname)s %(message)s')
 s3 = boto3.client('s3')
 
+
 def lambda_handler(event, context):
-    logger.info('This is an info message')
-    logger.warning('This is a warning message')
-    logger.error('This is an error message')    
-    
-    logger.info('event: %s', event)
-    
     event_type = event.get('detail-type')
     if event_type != 'Object Created':
         logger.warning('Not a supported event type: %s', event_type)
         return
-    
-    bucket_name =  event['detail']['bucket']['name']
+
+    bucket_name = event['detail']['bucket']['name']
     key = event['detail']['object']['key']
-    
+    return extract_frame(bucket_name, key)
+
+
+def video_api_handler(event, context):
+    logger.info('event: %s', event)
+    data: dict = json.loads(event['body'])
+    bucket_name = data['bucketName']
+    key = data['key']
+    time_off = data.get('timeOff')
+    time_off = time_off if time_off else '00:00:00'
+    ret = extract_frame(bucket_name, key, time_off)
+    ret['requestId'] = data.get('requestId')
+    return {
+        "statusCode": 200,
+        "body": json.dumps(ret)
+    }
+
+
+def extract_frame(bucket_name, key, time_off='00:00:00'):
     output_key = f"video-handler-output/{os.path.basename(key)}.jpg"
-    
+
     presigned_url = generate_presigned_url(bucket_name, key)
-    
+
     # 调用 FFprobe 获取视频时长
     ffprobe_command = f"ffprobe -v error -select_streams v:0 -show_entries format=duration -of default=noprint_wrappers=1:nokey=1 {presigned_url}"
     process = subprocess.Popen(ffprobe_command.split(), stdout=subprocess.PIPE)
     output, _ = process.communicate()
-        
+
     # 获取视频时长（以秒为单位）
     duration = float(output)
-        
+
     logger.info(f"Video duration: {duration} seconds")
-        
+
     # 调用FFmpeg提取第一帧图像
-    
-    ffmpeg_command = f"/opt/bin/ffmpeg -loglevel quiet  -i {presigned_url} -vframes 1 -f image2 -c:v mjpeg -"
+
+    ffmpeg_command = f"/opt/bin/ffmpeg -loglevel quiet -i {presigned_url} -ss {time_off} -vframes 1 -f image2 -c:v mjpeg -"
+    logger.info("Executed ffmpeg command: %s", ffmpeg_command)
     process = subprocess.Popen(ffmpeg_command.split(), stdout=subprocess.PIPE)
-    first_frame_data, _ = process.communicate()
-        
+    frame_data, _ = process.communicate()
+
     # 将图像上传到S3
-    s3.put_object(Body=first_frame_data, Bucket=bucket_name, Key=output_key, Metadata={'duration': f"{duration}"})
-        
-    logger.info(f"First frame image saved to: s3://{bucket_name}/{output_key}")
-    
+    s3.put_object(Body=frame_data, Bucket=bucket_name, Key=output_key, Metadata={'duration': f"{duration}"},
+                  ContentType='image/jpeg')
+
+    logger.info(f"Frame image saved to: s3://{bucket_name}/{output_key}")
+
     return {
         "statusCode": 200,
         "body": json.dumps({
-            "image": f"s3://{bucket_name}/{output_key}"
+            "originVideo": f"s3://{bucket_name}/{key}",
+            "image": f"s3://{bucket_name}/{output_key}",
+            "duration": duration,
         }),
     }
-    
+
+
 def generate_presigned_url(bucket_name, key, expiration=3600):
     response = s3.generate_presigned_url(
         'get_object',
